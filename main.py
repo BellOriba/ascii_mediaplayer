5ceb765d609ea0c2efae4c38fec249cfab5a8e2d
Gabriel Oliveira <gabrieloliveira.gos04@gmail.com>
2022-06-10 17:11:04 -0300

    Repository Setup

---
+++ main.py
@@ -0,0 +1,5 @@
+# ***********************************************
+# Transform images in ASCII art, runs GIFs and videos in command line with sound!
+#
+# ***********************************************
+print("Hello World!")
