9cc22c1a110c306a0b4cf9aabfbb8a93f141f592
Gabriel Oliveira <gabrieloliveira.gos04@gmail.com>
2022-06-11 00:21:32 -0300

    Setup Pillow library

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 ### TODO:
 - [ ] Add Image converter to ASCII in command line.
+- [ ] Add color to the image converter.
 - [ ] Add Video converter to ASCII in command line.
 - [ ] Add GIF converter to ASCII in command line.
 - [ ] Convert videos through youtube links.
--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-# ***********************************************
-# Transform images in ASCII art, runs GIFs and videos in command line with sound!
-#
-# ***********************************************
-print("Hello World!")
+from PIL import Image, ImageColor
+
+
+CHAR_LIST = ['░', '▒', '▓', '█']
+
+im = Image.open("img_test/Adamastor.png")
+print(im)
+
+## All PIL.Image attributes
+print("""
+filename = {}
+format = {}
+mode = {}
+size = {}
+width = {}
+height = {}
+palette = {}
+info = {}
+is_animated = {}
+n_frames = {}
+""".format(im.filename, im.format, im.mode, im.size, im.width, im.height, im.palette, im.info, im.is_animated, im.n_frames))
+
